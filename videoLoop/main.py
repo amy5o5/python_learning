e718c0f166f45c1f96a48596ec11d890f59ee663
mohammad amin <149660938+amy5o5@users.noreply.github.com>
2025-08-29 16:42:40 +0000

    get frames

--- videoLoop/main.py
+++ videoLoop/main.py
@@ -1,2 +1,15 @@
 import cv2
+import os
+your_video = "/video.mp4"
+cap = cv2.VideoCapture(your_video)
+frames = []
 
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+    frames.append(frame)
+
+cap.release()
+print(f"Total frames extracted: {len(frames)}")
+print(os.getcwd())
